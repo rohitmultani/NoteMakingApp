ce70bb7c147d63503f6bc02835a0c590559fc1df
Rohit Multani <rohitmultani@153.com>
2022-07-02 08:38:21 +0530

    edits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
 import {Fragment} from 'react'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './store/Data';
 import Home from './pages/Home';
@@ -14,12 +14,13 @@ function App() {
   return (
     <Fragment>
        <Provider store={store}>
-       <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Home />} />
+       {/* <BrowserRouter> */}
+    {/* <Routes> */}
+    <Home/>
+    {/* <Route path="/" element={<Home />} /> */}
       {/* <Route path="/:id" element={<Learn />} /> */}
-      </Routes>
-  </BrowserRouter>
+      {/* </Routes> */}
+  {/* </BrowserRouter> */}
     </Provider>
     </Fragment>
   )
