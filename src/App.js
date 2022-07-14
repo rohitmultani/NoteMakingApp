2750a1428fa9baad827ff2ee15d2f19829a87584
Rohit Multani <rohitmultani@153.com>
2022-07-14 16:16:36 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 
 import './App.css';
 import {Fragment} from 'react'
-// import {
-//   BrowserRouter,
-//   Routes,
-//   Route,
-// } from "react-router-dom";
+import {
+  BrowserRouter,  
+  Routes,
+  Route,
+} from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './store/Data';
 import Home from './pages/Home';
-// import Learn from './pages/Learn';
+import Learn from './pages/Learn';
 function App() {
   return (
     <Fragment>
        <Provider store={store}>
-       {/* <BrowserRouter> */}
-    {/* <Routes> */}
-    <Home/>
-    {/* <Route path="/" element={<Home />} /> */}
-      {/* <Route path="/:id" element={<Learn />} /> */}
-      {/* </Routes> */}
-  {/* </BrowserRouter> */}
+       <BrowserRouter>
+    <Routes>
+    {/* <Home/> */}
+    <Route path="/" element={<Home />} />
+      <Route path="/:id" element={<Learn />} />
+      </Routes>
+  </BrowserRouter>
     </Provider>
     </Fragment>
   )
